31ac29e8a2087563eabf809100eceee91a683399
Akash <akashnagpalnov@gmail.com>
2022-02-11 01:31:40 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Header">React Trello Clone</div>
+        <div className="Navbar">
+        <div className="Header" > Relevel Trello
+        </div>
+        
+      </div>
 
         <Board />
       </div>
